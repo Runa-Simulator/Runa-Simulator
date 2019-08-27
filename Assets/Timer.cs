fbd0dd9b3a774dadb85963ad7e793d1b6bdc6eeb
Runa PC <runa.simulator@fh-kiel.de>
2019-08-27 14:57:43 +0200

    Strafzeit
    Anzeige, Berechnung, wird im Ziel auf Gesamtzeit drauf gerechnet

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -5,27 +5,49 @@ using UnityEngine;
 public class Timer : MonoBehaviour {
 
     public GameObject Zeitanzeige;
-    TextMesh anzeige;
+    public GameObject Strafzeitanzeige;
+    TextMesh zeitanzeige;
+    TextMesh strafzeitanzeige;
     private float startTime;
     private bool start = false;
+    private float penaltyTime;
+    private bool penalty = false;
+    private float p = 0;
+    private float t = 0;
+    string minutes;
+    string seconds;
+    string p_minutes;
+    string p_seconds;
 
 	// Use this for initialization
 	void Start () {
-        anzeige = Zeitanzeige.GetComponent<TextMesh>();
-	}
+        zeitanzeige = Zeitanzeige.GetComponent<TextMesh>();
+        strafzeitanzeige = Strafzeitanzeige.GetComponent<TextMesh>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        
+        
         if (start)
         {
-            float t = Time.time - startTime;
+            t = Time.time - startTime;
+            minutes = ((int)t / 60).ToString();
+            seconds = (t % 60).ToString("f2");
 
-            string minutes = ((int)t / 60).ToString();
-            string seconds = (t % 60).ToString("f2");
+            zeitanzeige.text = minutes + " : " + seconds;
 
-            anzeige.text = minutes + " : " + seconds;
+            if (penalty)
+            {
+                p += Time.time - penaltyTime;
+                p_minutes = ((int)p / 60).ToString();
+                p_seconds = (p % 60).ToString("f1");
+
+                strafzeitanzeige.text = "+ " + p_minutes + " : " + p_seconds;
+            }
         }
-	}
+        penalty = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -40,7 +62,28 @@ public class Timer : MonoBehaviour {
         if (other.gameObject.CompareTag("Ziel"))
         {
             start = false;
-            anzeige.color = Color.yellow;
+            zeitanzeige.color = Color.yellow;
+
+            Debug.Log("Minuten: " + minutes);
+            Debug.Log("Sekunden: " + seconds);
+            Debug.Log("P Minuten: " + p_minutes);
+            Debug.Log("P Sekunden: " + p_seconds);
+
+            minutes = (((int)t / 60) + ((int)p / 60)).ToString();
+            seconds = ((t % 60) + (p % 60)).ToString("f2");
+
+            zeitanzeige.text = minutes + " : " + seconds;
+            strafzeitanzeige.text = " ";
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        // während Auto auf Wasser ist
+        if (other.gameObject.CompareTag("Wasser"))
+        {
+            Debug.Log("Strafzeit");
+            penalty = true;
+            penaltyTime = Time.time;
         }
     }
 }
